8f4b10904e0b843afcc74456c0c0f4e389f238db
Álvaro Lázaro <alvaro.lazaro.g@gmail.com>
2019-07-27 17:45:04 +0200

    Reorganise settings

--- padelanalytics/settings/dev.py
+++ padelanalytics/settings/dev.py
@@ -1,7 +1,9 @@
-from .prod import *
+from .common import *  # noqa: F403, F401
 
 DEBUG = True
 ALLOWED_HOSTS = []
 
 STATIC_URL = '/static/'
 PADEL_GOOGLE_TRACK_ID = None
+
+SECRET_KEY = 'adsadasdasd'
