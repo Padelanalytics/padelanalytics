abc62d1ceb22fcaaf4ea3ee1ea6709f8b8a8e20d
Álvaro Lázaro <alvaro.lazaro.g@gmail.com>
2019-08-07 14:53:23 +0200

    Add basic auth on Heroku

--- padelanalytics/settings/heroku_wsgi.py
+++ padelanalytics/settings/heroku_wsgi.py
@@ -7,5 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 from django.core.wsgi import get_wsgi_application
+from wsgi_basic_auth import BasicAuth
 
-application = get_wsgi_application()
+
+application = BasicAuth(get_wsgi_application())
