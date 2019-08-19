e24bbdab022d1ec82948f412d350290f3f09e31a
fragonser <fragonser@gmail.com>
2019-08-19 08:30:14 +0000

    Update tournaments_update.sql

--- tournaments/csv/tournaments_update.sql
+++ tournaments/csv/tournaments_update.sql
@@ -122,4 +122,7 @@ UPDATE tournaments_tournament SET club_id=11, date="2019-07-28", country="German
 UPDATE tournaments_tournament SET club_id=1, date="2019-08-04", country="Germany", city="Berlin" WHERE name="Berliner Open 2019 Kat. A" AND padel_serie="GPS-500";
 UPDATE tournaments_tournament SET club_id=1, date="2019-08-04", country="Germany", city="Berlin" WHERE name="Berliner Open 2019 Kat. B" AND padel_serie="GPS-100";
 UPDATE tournaments_tournament SET club_id=16, date="2019-08-04", country="Germany", city="Cuxhaven" WHERE name="Cuxhavener Open 2019" AND padel_serie="GPS-500";
+UPDATE tournaments_tournament SET club_id=8, date="2019-08-10", country="Germany", city="Worms" WHERE name="Wormser Open 2019" AND padel_serie="GPS-250";
+UPDATE tournaments_tournament SET club_id=2, date="2019-08-18", country="Germany", city="Hamburg" WHERE name="I Fip Promotion Padel Open Hamburg - Herren" AND padel_serie="GPS-1200" AND division="MO";
+UPDATE tournaments_tournament SET club_id=2, date="2019-08-18", country="Germany", city="Hamburg" WHERE name="I Fip Promotion Padel Open Hamburg - Damen" AND padel_serie="GPS-500" AND division="WO";
 COMMIT;
