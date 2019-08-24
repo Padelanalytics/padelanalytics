8ae9704052e49edde781f69017517631a8d5a73d
Álvaro Lázaro <alvaro.lazaro.g@gmail.com>
2019-08-24 21:15:36 +0200

    Fix winner calculation

---
+++ tests/tournaments/test_models.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from tournaments.models import PadelResult
+
+
+class PadelResultTestCase(TestCase):
+    def test_game_winner(self):
+        scores = [5, 7, 6, 3, 11, 9]
+        result = PadelResult.create(scores)
+        self.assertEqual(result.winner, 1)
