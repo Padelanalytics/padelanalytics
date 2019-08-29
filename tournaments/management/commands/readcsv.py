1ea635e42554a9757e23d1656646e222f4268fb3
Francisco Revilla <paconte@gmail.com>
2019-08-30 00:08:09 +0200

    Add new management method to assing clubs to a Person model class from a csv file.

--- tournaments/management/commands/readcsv.py
+++ tournaments/management/commands/readcsv.py
@@ -2,7 +2,7 @@
 
 This scripts allows the user to read csv files and import the readed data into the database.
 The idea is to have all the statidistical data in csv files (tournaments, players, rankings) and
-extend this files with new information (new tournament or ranking) or fix any mistake on the 
+extend this files with new information (new tournament or ranking) or fix any mistake on the
 information on the files (e.g. wrong player name or game result) and afterwards delete all the
 stadistical data from the database and import it from zero again.
 """
@@ -18,7 +18,7 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            'type', choices=['games', 'phases', 'stats_game', 'stats_tournament', 'padel', 'person', 'padel_ranking'])
+            'type', choices=['games', 'phases', 'stats_game', 'stats_tournament', 'padel', 'person', 'padel_ranking', 'player_club'])
         parser.add_argument('file_path', nargs='+')
 
     def handle(self, *args, **options):
@@ -46,12 +46,16 @@ class Command(BaseCommand):
             # import phases of a game
             reader = csvReader.CsvReader(csvReader.CsvReader.PHASE)
             reader.read_file(file_path)
-        elif csv_type == 'person':            
+        elif csv_type == 'person':
             reader = csvReader.CsvReader(csvReader.CsvReader.PERSON)
             reader.read_file(file_path)
         elif csv_type == 'padel_ranking':
             # imports a padel ranking, creating a persons while importing
             reader = csvReader.CsvReader(csvReader.CsvReader.PADEL_RANKING)
+            reader.read_file(file_path)
+        elif csv_type == 'player_club':
+            # add clubs to the player model
+            reader = csvReader.CsvReader(csvReader.CsvReader.PADEL_PLAYER_CLUB)
             reader.read_file(file_path)
         else:
             raise Exception('Argument %s not supported.' % csv_type)
