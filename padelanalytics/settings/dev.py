a0d8f93a4a12125c2e3bdcf44e14a8deaf8f476e
Francisco Javier Revilla Linares <paconte@gmail.com>
2019-09-01 11:28:58 +0000

    Revert "Merge branch 'refactor-settings' into 'master'"
    This reverts merge request !3

--- padelanalytics/settings/dev.py
+++ padelanalytics/settings/dev.py
@@ -1,9 +1,7 @@
-from .common import *  # noqa: F403, F401
+from .prod import *
 
 DEBUG = True
 ALLOWED_HOSTS = []
 
 STATIC_URL = '/static/'
 PADEL_GOOGLE_TRACK_ID = None
-
-SECRET_KEY = 'adsadasdasd'
