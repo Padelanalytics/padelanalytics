ddd14a9444d079bd61e1a45ebfae762a2239628e
Francisco Revilla <paconte@gmail.com>
2020-02-05 15:24:33 +0100

    WIP: improving booking courts js.

--- tournaments/static/js/booking.js
+++ tournaments/static/js/booking.js
@@ -1,12 +1,43 @@
 var selectedTimeSlots = [];
-var consecutiveSlots = 1;
+var slotPrice = 10;
+var totalPrice = 0;
+var maxSlots = 3;
+var minSlots = 2;
 var busyColor = "red";
 var freeColor = "green";
+var multiDay = false;
 
 
-function changeCellBackgroundColor(tableId, row, column, color) {
-    var table = document.getElementById(tableId);
-    table.rows[row].cells[column].style.backgroundColor = color;
+// Object related methods
+
+function getTableSlots(tableId) {
+    var result = [];
+    for (var i=0; i<selectedTimeSlots.length; i++){
+        if (selectedTimeSlots[i][0] == tableId){
+            result.push(selectedTimeSlots[i]);
+        }
+    }
+    return result;
+}
+
+
+function minNextConsecutivesSlots(num, matrix){
+    var result = [];
+    var maxRow = getMaxRowsTable(matrix[0][0]) - 1;
+    var highestRow = matrix[0][1]; // first row element
+    for (var i=1; i<matrix.length; i++) {
+        if (matrix[i][1] > highestRow){
+            highestRow = matrix[i][1];
+        }
+    }
+    var j = 0;
+    var row = highestRow + 1;
+    while (j < num && row <= maxRow){
+        result.push([matrix[0][0], row, matrix[0][2]]);
+        j++;
+        row++;
+    }
+    return result;
 }
 
 
@@ -17,6 +48,64 @@ function getSlotIndex(newSlot, slots){
         }
     }
     return -1;
+}
+
+
+function addOrRemoveSlot(tableId, row, column){
+    var slot = [tableId, row, column];
+    let index = getSlotIndex(slot, selectedTimeSlots);
+    if (index >= 0) {
+        for (var i = selectedTimeSlots.length-1; i >= 0; i--){
+            if (selectedTimeSlots[i][0] == tableId){
+                changeCellBackgroundColor(selectedTimeSlots[i][0], selectedTimeSlots[i][1], selectedTimeSlots[i][2], freeColor);
+                selectedTimeSlots.splice(i, 1);
+            }
+        }
+    } else if (index == -1) {
+        var tableSlots = getTableSlots(tableId);
+        if (tableSlots.length == 0){ // no selection => add min of possible selections
+            selectedTimeSlots.push(slot);
+            changeCellBackgroundColor(tableId, row, column, busyColor);
+            if (minSlots - 1 > 0){
+                var matrix = [slot];
+                var extraSlots = minNextConsecutivesSlots(minSlots-1, matrix);
+                for (var i=0; i<extraSlots.length; i++){
+                    selectedTimeSlots.push(extraSlots[i]);
+                    changeCellBackgroundColor(tableId, extraSlots[i][1], extraSlots[i][2], busyColor);
+                }
+            }
+        } else { // already a selection => check if possible to expand
+            var nextSlot = minNextConsecutivesSlots(1, tableSlots);
+            if (nextSlot.length == 1 && maxSlots > tableSlots.length && nextSlot[0][0] == tableId && nextSlot[0][1] == row && nextSlot[0][2] == column) {
+                selectedTimeSlots.push(nextSlot[0]);
+                changeCellBackgroundColor(tableId, nextSlot[0][1], nextSlot[0][2], busyColor);
+            }
+        }
+        updatePrice();
+        //selectedTimeSlots.push(slot);
+        //changeCellBackgroundColor(tableId, row, column, busyColor);
+    }
+}
+
+
+function updatePrice() {
+    totalPrice = slotPrice * selectedTimeSlots.length;
+    var htmlPrice = document.getElementById("totalPrice");
+    htmlPrice.innerHTML = totalPrice;
+}
+
+
+// table methods
+
+function getMaxRowsTable(tableId) {
+    var table = document.getElementById(tableId);
+    return table.rows.length;
+}
+
+
+function changeCellBackgroundColor(tableId, row, column, color) {
+    var table = document.getElementById(tableId);
+    table.rows[row].cells[column].style.backgroundColor = color;
 }
 
 
@@ -35,17 +124,7 @@ function bindOnClickMethod(tableId){
 
 
 function easyPeasy(tableId, row, column){
-    var slot = [tableId, row, column];
-    let index = getSlotIndex(slot, selectedTimeSlots);
-
-    if (index < 0){
-        selectedTimeSlots.push(slot);
-        changeCellBackgroundColor(tableId, row, column, busyColor);
-    } else {
-        selectedTimeSlots.splice(index, 1);
-        changeCellBackgroundColor(tableId, row, column, freeColor);
-    }
-    console.log(selectedTimeSlots);
+    addOrRemoveSlot(tableId, row, column);
 }
 
 
