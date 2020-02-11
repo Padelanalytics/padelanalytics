d5f794c320208252327097ecd50d4883191c13d5
Francisco Revilla <paconte@gmail.com>
2020-02-11 15:38:58 +0100

    Second version of Booking models. Only with comments.

--- booking/models.py
+++ booking/models.py
@@ -2,17 +2,29 @@ from django.db import models
 
 
 # Create your models here.
-class ItemOwner(models.Model):
+class Merchant(models.Model):
+    """
+    Model to contain information about the merchant
+
+    :name: Name of the merchant.
+    :address: Address of the merchant.
+    :phone: Phone contact of the merchant.
+    :email: Emais of the merchant.
+    """
     pass
 
 
 class BookingItem(models.Model):
     """
-    Model to connect a booking with a related object.
+    Model to contain information about an available Item to be booked.
 
-    :quantity: Quantity of booked items.
-    :subtotal (optional): Field for storing the price of each individual item.
-    :booked_item: Connection to related booked item.
+    :quantity: Quantity of available items.
+    :price: Field for storing the price of each individual item.
+    :currency (optional): If total is uses, we usually also need a currency.
+    :email: Email of contact when the Item is booked.
+    :active: if is available to be booked.
+    :merchant: Connection to related merchant
+    :bookingConfig: Connection to related booking configuration.
     :booking: Connection to related booking.
 
     properties:
@@ -21,19 +33,32 @@ class BookingItem(models.Model):
     pass
 
 
+class BookingConfig(models.Model):
+    """
+    Model to contain information about the configuration of a bookingItem.
+
+    :bookingItem: Connection to related bookingItem.
+    :date_from: From when the item is available.
+    :date_until: Until when the item is available.
+    :time_slot: slots to be displayed to the user when booking.
+    :max_slot: max number of consecutive slots to be booked.
+    :min_slot: min number of consevutive slots to be booked.
+    """
+    pass
+
+
 class Booking(models.Model):
     """
-    Model to contain information about a booking.
+    Model to contain information about the booking.
 
     :forename (optional): First name of the user.
     :surname (optional): Last name of the user.
     :email: Email of the user.
+    :creation_date: Date of the booking.
     :date_from (optional): From when the booking is active.
     :date_until (optional): Until when the booking is active.
     :time_period (optional): How long the period from date_from will be.
-    :creation_date: Date of the booking.
-    :total (optional): Field for storing a total of all items.
-    :currency (optional): If total is uses, we usually also need a currency.
-
+    :slots: How many slots.
+    :price: Total price of the booking.
     """
     pass
