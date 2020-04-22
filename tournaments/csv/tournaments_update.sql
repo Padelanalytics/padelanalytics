3d3aa51f8b708191ff8c22ef8827a05ec4cbd043
fragonser <fragonser@gmail.com>
2020-04-22 23:44:28 +0000

    Update tournaments_update.sql with 2x tournaments in 2020

--- tournaments/csv/tournaments_update.sql
+++ tournaments/csv/tournaments_update.sql
@@ -243,4 +243,6 @@ UPDATE tournaments_tournament SET club_id=68, date="2019-11-09", country="Nether
 UPDATE tournaments_tournament SET club_id=68, date="2019-11-09", country="Netherlands", city="Joure" WHERE name="NPB-100 dagtoernooi Joure - Dames" AND padel_serie="NPB-100-2K" AND division="WO";
 UPDATE tournaments_tournament SET club_id=55, date="2019-11-24", country="Netherlands", city="Rijswijk" WHERE name="FIP STAR LA PLAYA" AND padel_serie="FIP-STAR" AND division="MO";
 UPDATE tournaments_tournament SET club_id=55, date="2019-11-24", country="Netherlands", city="Rijswijk" WHERE name="FIP STAR LA PLAYA" AND padel_serie="FIP-STAR" AND division="WO";
+UPDATE tournaments_tournament SET club_id=??, date="2020-03-08", country="World Padel Tour", city="Marbella" WHERE name="Cervezas Victoria Marbella Master 2020 - Femenino" AND padel_serie="Master" AND division="WO";
+UPDATE tournaments_tournament SET club_id=??, date="2020-03-08", country="World Padel Tour", city="Marbella" WHERE name="Cervezas Victoria Marbella Master 2020 - Masculino" AND padel_serie="Master" AND division="MO";
 COMMIT;
