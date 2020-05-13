4375aa67899a9d4ce1d7829283d234f333ba2995
Francisco Revilla <paconte@gmail.com>
2020-05-13 18:20:19 +0200

    REST: add urls for the new rest api.

--- tournaments/urls.py
+++ tournaments/urls.py
@@ -6,13 +6,19 @@ from django.conf.urls import url
 from django.conf.urls import include
 from django.conf.urls.static import static
 
+from rest_framework import routers
+
 from tournaments import views
 
 
 handler404 = views.handler404
 handler500 = views.handler500
 
+router = routers.DefaultRouter()
+router.register(r'ranking', views.PadelRankingSet)
+
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('i18n/', include('django.conf.urls.i18n')),
     #path('test_view', views.test_view, name='test_view'),
     url(r'^$', views.index, name='index'),
