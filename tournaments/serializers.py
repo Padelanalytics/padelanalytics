be9270d02c432ec92146967bf05fc37f8334b59e
Francisco Revilla <paconte@gmail.com>
2020-05-13 18:18:02 +0200

    REST: Add first version of serializers.py

---
+++ tournaments/serializers.py
@@ -0,0 +1,16 @@
+from tournaments.models import PadelRanking
+from tournaments.models import Person
+
+from rest_framework import serializers
+
+
+class PersonSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Person
+        fields = ['first_name', 'last_name']
+
+
+class PadelRankingSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = PadelRanking
+        fields = ['date', 'points', 'position']
